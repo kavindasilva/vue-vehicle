644cb0fcfd64a177fd63aeda6fefabd1fa1d4aa0
Kavinda Silva <kss.kavinda@gmail.com>
2020-09-17 17:32:16 +0530

    checnges from code-with-yasith

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,71 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>Essential Links</h2>
+
+    <h2>Ecosystem</h2>
+
+    <input type="text" id="txt1" v-model="txt2" /><br/> <!-- realtime update -->
+    <input type="text" id="txt2" v-model.lazy="txt2" /><br/> <!-- change on enter/click outside-->
+
+    <button v-on:click="age++">Btn1</button>
+    <button v-on:click="addA()">Btn2</button>
+    <button v-on:click="isValid = !isValid" v-bind:class="{cssAvailable: isValid, cssNotAvailable: !isValid}" >Btn3</button>
+    <button @dblclick="isValid = !isValid" v-bind:class="{cssAvailable: isValid, cssNotAvailable: !isValid}" >Btn3</button>
+
+    <div>
+      <table style="margin: 0 auto; align: center">
+        <tr><td>Text </td><td> {{ input_txt1 }} </td></tr>
+        <tr><td>Text2 </td><td> {{ name }} </td></tr>
+        <tr><td>Text3 </td><td> {{ msg }} </td></tr>
+        <tr><td>Text3 </td><td> {{ age }} </td></tr>
+        <tr><td>2waybind </td><td> {{ txt2 }} </td></tr>
+        <tr><td>computed </td><td> {{ addAs }} </td></tr>
+        <tr><td>isValid </td><td> {{ isValid }} </td></tr>
+      </table>
+    </div>
+    <div id="canvas" style="height:200px; width:200px; border: 1px solid red;" v-on:mousemove="updateXY">{{ x }}, {{ y }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'HelloWorld-ww',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js Appx',
+      name: 'kk',
+      input_txt1: '',
+      txt2: 'Welcome2',
+      age: 4,
+      x: 99,
+      y: 99,
+      isValid: false
+    }
+  },
+  methods: {
+    addA: function () {
+      console.log('addA')
+      return this.x + this.y
+    },
+    updateXY (event) { // Function to update the X and Y cordinated of the mouse position
+      this.x = event.offsetX
+      this.y = event.offsetY
+    }
+  },
+  computed: {
+    addAs: function () {
+      console.log('addAs-computed')
+      return this.x + this.y
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1, h2 {
+  font-weight: normal;
 }
 ul {
   list-style-type: none;
@@ -55,5 +77,11 @@ li {
 }
 a {
   color: #42b983;
+}
+.cssAvailable{
+  background-color: green;
+}
+.cssNotAvailable{
+  background-color: red;
 }
 </style>
