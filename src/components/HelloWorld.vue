1b08938b3863333632b5dafaec6b38a6fa819157
kavindasilva <kss.kavinda@gmail.com>
2020-09-18 21:38:14 +0530

    WIP unit test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,93 +2,68 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
+
     <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+
+    <input type="text" id="txt1" v-model="txt2" ref="inputRef"/><br/> <!-- realtime update -->
+    <input type="text" id="txt2" v-model.lazy="txt2" /><br/> <!-- change on enter/click outside-->
+
+    <button v-on:click="age++">Btn1</button>
+    <button v-on:click="addA()">Btn2</button>
+    <button v-on:click="isValid = !isValid" v-bind:class="{cssAvailable: isValid, cssNotAvailable: !isValid}" >Btn3</button>
+    <button v-on:click="readRef">Btn4</button>
+    <button @dblclick="isValid = !isValid" v-bind:class="{cssAvailable: isValid, cssNotAvailable: !isValid}" >Btn3</button>
+
+    <div>
+      <table style="margin: 0 auto; align: center">
+        <tr><td>Text </td><td> {{ input_txt1 }} </td></tr>
+        <tr><td>Text2 </td><td> {{ name }} </td></tr>
+        <tr><td>Text3 </td><td> {{ msg }} </td></tr>
+        <tr><td>Text3 </td><td> {{ age }} </td></tr>
+        <tr><td>2waybind </td><td> {{ txt2 }} </td></tr>
+        <tr><td>computed </td><td> {{ addAs }} </td></tr>
+        <tr><td>isValid </td><td> {{ isValid }} </td></tr>
+        <tr v-if="isValid" style="background-color: yellow"><td>SHOW-isValid </td><td> {{ isValid }} </td></tr>
+        <tr v-else style="background-color: purple"><td>!SHOW-isValid </td><td> {{ isValid }} </td></tr>
+        <tr v-show="isValid" style="background-color: yellow"><td>SHOW-isValid </td><td> {{ isValid }} </td></tr>
+      </table>
+    </div>
+    <div id="canvas" style="height:200px; width:200px; border: 1px solid red;" v-on:mousemove="updateXY">{{ x }}, {{ y }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'HelloWorld-ww',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js Appx',
+      name: 'kk',
+      input_txt1: '',
+      txt2: 'Welcome2',
+      age: 4,
+      x: 99,
+      y: 99,
+      isValid: false
+    }
+  },
+  methods: {
+    addA: function () {
+      console.log('addA')
+      return this.x + this.y
+    },
+    updateXY (event) { // Function to update the X and Y cordinated of the mouse position
+      this.x = event.offsetX
+      this.y = event.offsetY
+    },
+    readRef: function(){
+      console.log(this.$refs.inputRef.value);
+    }
+  },
+  computed: {
+    addAs: function () {
+      console.log('addAs-computed')
+      return this.x + this.y
     }
   }
 }
@@ -109,5 +84,11 @@ li {
 }
 a {
   color: #42b983;
+}
+.cssAvailable{
+  background-color: green;
+}
+.cssNotAvailable{
+  background-color: red;
 }
 </style>
