37479a9b1bcabbe88dbe72d6571757a834eacfe0
kavindasilva <kss.kavinda@gmail.com>
2020-09-23 22:41:27 +0530

    computed values

--- src/components/CheatSheet1.vue
+++ src/components/CheatSheet1.vue
@@ -2,14 +2,22 @@
   <div class="hello">
     <h2>Cheat Sheet 1</h2>
     <h4 id="count_head">Counter now: {{ count }}</h4>
+    <h6 id="count_head">Counter sqr: {{ comCountSquare }}</h6>
+    <span>Count threshold: <input type="text" v-model.number="countThreshold"/></span> <br/>
 
-    <button id="inc_count" v-on:click="count++">Increase</button> <hr/>
-    
+    <button id="inc_count" v-on:click="count++">Increase</button>
+    <button id="reset_count" v-on:click="count=0">0</button>
+    <button id="dec_count" v-on:click="count--">Decrease</button> <hr/>
+
     <a v-on:click.prevent="logClick" href="http://www.google.lk">Google - prevent click event</a> <br/>
     <a v-on:click="logClick" href="http://www.google.com">Google - allow click event</a> <hr/>
 
     <p v-html="webTag"></p>
-    <span v-html="webTag"></span>
+    <span v-html="webTag"></span> <hr/>
+
+    Update lazy - -: <input type="text" v-model.lazy="inputText1"/>
+    Update realtime: <input type="text" v-model="inputText1"/> <br/>
+    One way bind: <input type="text" v-bind:value="inputText1"/>
 
   </div>
 </template>
@@ -19,6 +27,8 @@ export default {
   data () {
     return {
       count: 0,
+      inputText1: 'x',
+      countThreshold: 0,
       webTag: '<a href="http://www.google.com/">Web tag text</a>',
     }
   },
@@ -28,6 +38,9 @@ export default {
     }
   },
   computed: {
+    comCountSquare(){
+      return this.count * this.count
+    }
   }
 }
 </script>
