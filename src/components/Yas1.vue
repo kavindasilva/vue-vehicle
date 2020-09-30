2bb94c00bb0e3f9d175ffe1a24043ebf62642391
Kavinda Silva <kavinda@vetstoria.com>
2020-09-30 17:56:57 +0530

    props and events

--- src/components/Yas1.vue
+++ src/components/Yas1.vue
@@ -1,5 +1,6 @@
 <template>
-    <div v-theme:column="getWdieNarrow">
+    <div v-theme:column="getWideOrNarrow">
+        <com-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></com-header>
         <button @click="isWide = !isWide">Wide/Narrow</button>
         <h1>Posts</h1>
         <div v-for="(post, i) in posts" v-bind:key="i">
@@ -8,15 +9,18 @@
             <article>{{ post.body }}</article>
         </div>
     </div>
+    <!-- @TODO: check https://bitbucket.org/Yasith17/spa/src/lesson-30/vue-app/src/components/ -->
 </template>
 
 <script>
+import Head from "../components/head"
 export default {
   name: 'App',
     data(){
         return{
             posts: [],
-            isWide: true
+            isWide: true,
+            title: 'hhhh'
         }
     },
     created(){
@@ -27,10 +31,16 @@ export default {
     methods: {
         toUpperCaseC (param) {
             return param.toUpperCase();
+        },
+        updateTitle: function(updateTitle){
+            this.title = updateTitle;
         }
     },
+    components: {
+        'com-header': Head
+    },
     computed: {
-        getWdieNarrow () {
+        getWideOrNarrow () {
             return this.isWide ? 'wide' : 'narrow';
         }
     }
---
+++ src/components/head.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1 v-on:click="changeTitle">{{ title }}</h1>
+    </header>
+</template>
+<script>
+export default {
+    props: {
+        title: {
+            type: String
+        }
+    },
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        changeTitle: function(){
+            let randd = Math.random()
+            this.$emit('changeTitle', randd.toString());
+        }
+    }
+}
+</script>
